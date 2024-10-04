a9a17a37966ad281f242bb3cfef7c98d065df7d3
RafikoZZ555 <rafalek.maseraczek@gmail.com>
2024-10-04 11:21:38 +0200

    Add files via upload

---
+++ Projekt/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+       System.out.println("Podaj swoje imie ");
+       String name = scanner.nextLine();
+        System.out.println("Podaj swoje imie ");
+        String surname = scanner.nextLine();
+        System.out.println("Witaj " + name + " " + surname);
+        System.out.println("podaj swoją klasę ");
+        String Shoolclass =scanner.nextLine();
+        System.out.println("Chodzisz do kalsy " + Shoolclass);-m
+
+        }
+    }
