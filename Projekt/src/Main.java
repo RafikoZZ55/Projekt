1c71781b4a5c36c89ead62b8e3a36ddce43e8e43
Your Name <rafalek.maseraczek@gmail.com>
2024-10-04 11:51:27 +0200

    first commit

--- Projekt/src/Main.java
+++ Projekt/src/Main.java
@@ -1,19 +1,33 @@
 import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-       System.out.println("Podaj swoje imie ");
-       String name = scanner.nextLine();
+        System.out.println("Podaj swoje imie ");
+        String name = scanner.nextLine();
         System.out.println("Podaj swoje imie ");
         String surname = scanner.nextLine();
         System.out.println("Witaj " + name + " " + surname);
         System.out.println("podaj swoją klasę ");
-        String Shoolclass =scanner.nextLine();
-        System.out.println("Chodzisz do kalsy " + Shoolclass);-m
+        String Shoolclass = scanner.nextLine();
+        System.out.println("Chodzisz do kalsy " + Shoolclass);
 
+        while (true ) {
+        System.out.println("Podaj liczbę wiazd w obzerwowalnym wszechświecie");
+        int gwiazdy = scanner.nextInt();
+            if (gwiazdy == 300) {
+                System.out.println("Wowo jesteś matemaksem");
+                break;
+            } else if (gwiazdy >= 300) {
+                System.out.println("Za dużo");
+                continue;
+            } else if (gwiazdy <= 300) {
+                System.out.println("Za mało");
+                continue;
+            } else {
+                System.out.println("Coś zepsułeś!");
+                break;
+            }
         }
     }
+}
